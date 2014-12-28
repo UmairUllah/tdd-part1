16b25d1233c0f191ec3a9c062c28ba85c5ea07ed
UmairUllah <umairullah11@yahoo.com>
2014-12-28 13:09:21 +0500

    Chapter 2: Extending testMuliplication method (Failed) 		   Moving the code of Dollar class to new java file.

--- tdd-part1/src/TestWyCash.java
+++ tdd-part1/src/TestWyCash.java
@@ -12,19 +12,8 @@ public class TestWyCash extends TestCase {
 		Dollar five = new Dollar(5);
 		five.times(2);
 		assertEquals(10, five.amount);
-	}
+		five.times(3);
+		assertEquals(15, five.amount);
+	}	
 	
-	class Dollar {
-		int amount;
-		
-		public Dollar( int amount) {
-			this.amount = amount;
-		}
-		
-		public void times(int multiplier) {
-			amount *= multiplier;
-		}
-		
-	}
-
 }
