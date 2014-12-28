1e07a06de7807ebfd4acb408d7f468fff0a778ae
UmairUllah <umairullah11@yahoo.com>
2014-12-28 12:12:40 +0500

    Chapter 1: First Test (Fail).

---
+++ tdd-part1/src/TestWyCash.java
@@ -0,0 +1,17 @@
+import org.junit.*;
+
+import org.junit.Test;
+
+import junit.framework.TestCase;
+
+
+public class TestWyCash extends TestCase {
+
+	@Test
+	public void testMultiplication() {
+		Dollar five = new Dollar(5);
+		five.times(2);
+		assertEquals(10, five.amount);
+	}
+
+}
