1391375490043fece3ba52a32892215e91a5f659
UmairUllah <UmairUllah@users.noreply.github.com>
2014-12-29 15:36:29 +0500

    Chapter 6: Removing equals method from Franc to use Common Equals

--- tdd-part1/src/Franc.java
+++ tdd-part1/src/Franc.java
@@ -1,6 +1,5 @@
 
-public class Franc {
-	public int amount;
+public class Franc extends Money{
 
 	public Franc(int amount) {
 		this.amount = amount;
@@ -8,10 +7,5 @@ public class Franc {
 	
 	public Franc times( int multiplier ) {
 		return new Franc(amount * multiplier);
-	}
-	
-	public boolean equals( Object object ){
-		Franc franc = (Franc) object;
-		return amount == franc.amount;
 	}
 }
