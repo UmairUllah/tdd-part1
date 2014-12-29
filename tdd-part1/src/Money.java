18215f0fc326a21fc9f69298fc44bc56a019964c
UmairUllah <UmairUllah@users.noreply.github.com>
2014-12-29 17:02:54 +0500

    Chapter 8: Modifying Money.java
    - Making Money class abstract .
    - Adding static factory methods to return new Dollar and Franc objects.
    - Added a abstract method times that return Money object.

--- tdd-part1/src/Money.java
+++ tdd-part1/src/Money.java
@@ -1,11 +1,21 @@
 
-public class Money {
+public abstract class Money {
  protected int amount;
  
 	public boolean equals( Object object ){
 		Money money = (Money) object;
 		return amount == money.amount
-		  && getClass().equals(money.getClass());
+			&& getClass().equals(money.getClass());
 	}
+
+	public static Dollar dollar(int amount) {
+		return new Dollar(amount);
+	}
+	
+	public static Franc franc(int amount) {
+		return new Franc(amount);
+	}
+	
+	abstract Money times(int multiplier);
 	
 }
