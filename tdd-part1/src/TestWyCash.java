86ce5fa9d09e8ad28f4e827665a3e3d967371263
UmairUllah <UmairUllah@users.noreply.github.com>
2014-12-30 12:25:53 +0500

    Chapter 11: Added test to check different class equality.

--- tdd-part1/src/TestWyCash.java
+++ tdd-part1/src/TestWyCash.java
@@ -25,7 +25,7 @@ public class TestWyCash extends TestCase {
 	
 	@Test
 	public void testFrancMultiplication() {
-		Franc five = new Franc(5);
+		Money five = new Franc(5);
 		assertEquals(Money.franc(10), five.times(2));
 		assertEquals(Money.franc(15), five.times(3));
 	}
@@ -34,5 +34,10 @@ public class TestWyCash extends TestCase {
 	public void testCurrency(){
 		assertEquals("USD", Money.dollar(1).currency());
 		assertEquals("CHF", Money.franc(1).currency());
+	}
+	
+	@Test
+	public void testDifferentClassEqaulity() {
+		assertTrue(new Money(10,"CHF").equals(new Franc(10,"CHF")));
 	}
 }
