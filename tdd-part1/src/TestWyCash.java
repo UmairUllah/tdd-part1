45fc9b2f441fa889bcf7e14acbf85ef98a08c01d
UmairUllah <UmairUllah@users.noreply.github.com>
2015-01-02 12:49:30 +0500

    Bug removal: Forget to remove call of Dollar's contructor.

--- tdd-part1/src/TestWyCash.java
+++ tdd-part1/src/TestWyCash.java
@@ -6,7 +6,7 @@ public class TestWyCash extends TestCase {
 
 	@Test
 	public void testMultiplication() {
-		Dollar five = new Dollar(5);
+		Dollar five = Money.dollar(5);
 		assertEquals(Money.dollar(10), five.times(2));
 		assertEquals(Money.dollar(15), five.times(3));
 	}	
