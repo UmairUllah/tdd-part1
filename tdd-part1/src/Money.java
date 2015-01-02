4663cd2bc3b8843dd3eb0e9ff027f3a68995f023
UmairUllah <UmairUllah@users.noreply.github.com>
2015-01-02 12:20:39 +0500

    Chapter 15: Modified Money.java
    - Returned Expression objects from times and plus methods.

--- tdd-part1/src/Money.java
+++ tdd-part1/src/Money.java
@@ -26,7 +26,7 @@ public class Money implements Expression {
 		return currency;
 	}
 	
-	public Money times( int multiplier ) {
+	public Expression times( int multiplier ) {
 		return new Money(amount * multiplier, currency);
 	}
 	
@@ -36,12 +36,13 @@ public class Money implements Expression {
 		return amount + " " + currency;
 	}
 	
-	public Expression plus(Money addend) {
+	public Expression plus(Expression addend) {
 		return new Sum( this, addend );
 	}
 	
-	public Money reduce( String to ) {
-		return this;
+	public Money reduce( Bank bank,String to ) {
+		int rate = bank.rate(currency, to);
+		return new Money(amount/rate,to);
 	}
 	
 }
